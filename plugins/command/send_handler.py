4fd9abca197ea9b65468c01c9173568f0c2c9808
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-09 14:57:35 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -74,14 +74,14 @@ async def send_menfess_handler(client: Client, msg: types.Message):
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
         await db.update_menfess(coin, menfess, all_menfess)
-    keyboard = [
-        [InlineKeyboardButton(                "ᴛᴏᴘ ᴄᴏɪɴ💰", url="https://t.me/topupcoinbot?start=start")],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
     await msg.reply(pesan, True, enums.ParseMode.HTML, reply_markup=reply_markup)
         await msg.reply(f"Pesan anda <a href='{link + str(kirim.id)}'>berhasil terkirim.</a> \n\nhari ini kamu telah mengirim pesan sebanyak {menfess + 1}/{config.batas_kirim}. kamu dapat mengirim pesan sebanyak {config.batas_kirim} kali dalam sehari. \n\nwaktu reset setiap jam 1 pagi")
     else:
         await msg.reply('media yang didukung photo, video dan voice')
+    keyboard = [
+        [InlineKeyboardButton(                "ᴛᴏᴘ 💰", url="https://t.me/topupcoinbot?start=start")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
 async def get_link():
     anu = str(config.channel_1).split('-100')[1]
