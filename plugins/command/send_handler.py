d88e1639cf99c59e39148dbdaf2503a36250648f
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-09 16:37:21 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -45,7 +45,7 @@ async def send_menfess_handler(client: Client, msg: types.Message):
     db = Database(msg.from_user.id)
     db_user = db.get_data_pelanggan()
     db_bot = db.get_data_bot(client.id_bot).kirimchannel
-    keyboard = [InlineKeyboardButton(                "ᴛᴏᴘ ᴄᴏɪɴ💰", url="https://t.me/topupcoinbot?start=start")],
+    keyboard = [InlineKeyboardButton(                "lihat pesan", url="<a href='{link + str(kirim.id)}'</a>")],
     reply_markup = InlineKeyboardMarkup(keyboard)
     if msg.text or msg.photo or msg.video or msg.voice:
         if msg.photo and not db_bot.photo:
