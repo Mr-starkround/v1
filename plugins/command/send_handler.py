85c4a9722516f6e9158e442f0de117081029d54a
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-09 16:41:05 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -45,7 +45,7 @@ async def send_menfess_handler(client: Client, msg: types.Message):
     db = Database(msg.from_user.id)
     db_user = db.get_data_pelanggan()
     db_bot = db.get_data_bot(client.id_bot).kirimchannel
-    keyboard = [InlineKeyboardButton(                "lihat pesan", url="<a href='{link + str(kirim.id)}'</a>")],
+    keyboard = [InlineKeyboardButton(                "lihat pesan", url="https://t.me/topupcoinbot?start=start")],
     reply_markup = InlineKeyboardMarkup(keyboard)
     if msg.text or msg.photo or msg.video or msg.voice:
         if msg.photo and not db_bot.photo:
@@ -72,11 +72,12 @@ async def send_menfess_handler(client: Client, msg: types.Message):
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
         await db.update_menfess(coin, menfess, all_menfess)
+    keyboard = [InlineKeyboardButton(                "lihat pesan", url="https://t.me/topupcoinbot?start=start")],
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
         await msg.reply(f"Pesan anda <a href='{link + str(kirim.id)}'>berhasil terkirim.</a> \n\nhari ini kamu telah mengirim pesan sebanyak {menfess + 1}/{config.batas_kirim}. kamu dapat mengirim pesan sebanyak {config.batas_kirim} kali dalam sehari. \n\nwaktu reset setiap jam 1 pagi", True, enums.ParseMode.HTML, reply_markup=reply_markup)
     else:
         await msg.reply('media yang didukung photo, video dan voice')
-  
-    await msg.reply(pesan, True, enums.ParseMode.HTML, reply_markup=reply_markup)
 
 async def get_link():
     anu = str(config.channel_1).split('-100')[1]
