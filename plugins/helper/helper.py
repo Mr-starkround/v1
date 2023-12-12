5c140597f39ca5b901efa6165e6c3c4369dbe3cf
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-13 01:19:34 +0700

    Update helper.py

--- plugins/helper/helper.py
+++ plugins/helper/helper.py
@@ -46,7 +46,8 @@ class Helper():
         link_1 = await self.bot.export_chat_invite_link(config.channel_1)
         link_2 = await self.bot.export_chat_invite_link(config.channel_2)
         markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ ʙᴀsᴇ', url=link_1), InlineKeyboardButton('ɢʀᴏᴜᴘ ʙᴀsᴇ', url=link_2)],
+InlineKeyboardButton('ɢʀᴏᴜᴘ ʙᴀsᴇ', url=link_2)],
+            [InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ ʙᴀsᴇ', url=link_1),
             [InlineKeyboardButton('ᴄᴏʙᴀ ʟᴀɢɪ', url='https://t.me/jawafessbot?start=start')]
         ])
         await self.bot.send_message(self.user_id, config.pesan_join, reply_to_message_id=self.message.id, reply_markup=markup)
