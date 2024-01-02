5b53cb82336365467e4d1bb9641fa76960e44507
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-02 16:56:53 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -81,11 +81,7 @@ async def send_menfess_handler(client: Client, msg: types.Message, hastag: list,
                     "🗑ʜᴀᴘᴜs",
                     callback_data="hps")
             ],
-        ]
-        caption = msg.text or msg.caption
-        entities = msg.entities or msg.caption_entities
-
-        kirims = await client.send_photo(config.channel_1, msg.from_user.id, msg.id)
+        ]        
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
         await db.update_menfess(coin, menfess, all_menfess)
         await msg.reply(f"Pesan anda <a href='{link + str(kirim.id)}'>berhasil terkirim.</a> \n\nhari ini kamu telah mengirim pesan sebanyak {menfess + 1}/{config.batas_kirim}. kamu dapat mengirim pesan sebanyak {config.batas_kirim} kali dalam sehari. \n\nwaktu reset setiap jam 1 pagi",       
@@ -95,12 +91,7 @@ async def send_menfess_handler(client: Client, msg: types.Message, hastag: list,
  ),
 
     else:
-        await msg.reply(f"Pesan anda <a href='{link + str(kirims.id)}'>berhasil terkirim.</a> \n\nhari ini kamu telah mengirim pesan sebanyak {menfess + 1}/{config.batas_kirim}. kamu dapat mengirim pesan sebanyak {config.batas_kirim} kali dalam sehari. \n\nwaktu reset setiap jam 1 pagi",       
-
-       disable_web_page_preview=True,        reply_markup=InlineKeyboardMarkup(buttons),
-        quote=True
- ),    
-        return await msg.reply('media yang didukung photo, video dan voice')
+        await msg.reply('media yang didukung photo, video dan voice')
 
 async def get_link():
     anu = str(config.channel_1).split('-100')[1]
