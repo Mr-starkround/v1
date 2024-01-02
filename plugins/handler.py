701adfe1f91c254e0e7fd67401bbe057ad937c81
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-02 16:26:45 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -163,7 +163,7 @@ async def on_message(client: Client, msg: Message):
                         if key == command.lower()
                         or len(command.split(' ')) < 3
                         else await send_menfess_handler(
-                            client, msg, key, hastag
+                            client, msg
                         )
                     )
                 elif key in hastag:
