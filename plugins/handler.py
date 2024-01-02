dd5750bd63cb4908b6036932d744184bddb9ffc7
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-02 19:21:15 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -171,9 +171,9 @@ async def on_message(client: Client, msg: Message):
                         return await msg.reply('⚠️  post gagal terkirim, <b>mengirim pesan wajib lebih dari 3 kata.</b>', True, enums.ParseMode.HTML)
                     else:
                         return await send_menfess_handler(client, msg, hastag)
-           else:
+                    else:
                 await send_menfess(client, msg)
-                else:
+                   else:
                     await gagal_kirim_handler(client, msg)
 
     elif msg.chat.type == enums.ChatType.SUPERGROUP:
