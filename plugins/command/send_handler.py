4ef6ac625975490583cf3073182ed5953367de48
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-02 16:40:09 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -82,6 +82,10 @@ async def send_menfess_handler(client: Client, msg: types.Message, hastag: list,
                     callback_data="hps")
             ],
         ]
+        caption = msg.text or msg.caption
+        entities = msg.entities or msg.caption_entities
+
+        kirims = await client.send_photo(config.channel_1, picture, caption, caption_entities=entities)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
         await db.update_menfess(coin, menfess, all_menfess)
         await msg.reply(f"Pesan anda <a href='{link + str(kirim.id)}'>berhasil terkirim.</a> \n\nhari ini kamu telah mengirim pesan sebanyak {menfess + 1}/{config.batas_kirim}. kamu dapat mengirim pesan sebanyak {config.batas_kirim} kali dalam sehari. \n\nwaktu reset setiap jam 1 pagi",       
