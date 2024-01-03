dcb68f5edd913c5f391691b6fb7c00ebc84e2786
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-03 09:35:38 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -163,14 +163,14 @@ async def on_message(client: Client, msg: Message):
                         if key == command.lower()
                         or len(command.split(' ')) < 3
                         else await menfess_handler(
-                            client, msg, hastag
+                            client, msg
                         )
                     )
                 elif key in hastag:
                     if key == command.lower() or len(command.split(' ')) < 3:
                         return await msg.reply('⚠️  post gagal terkirim, <b>mengirim pesan wajib lebih dari 3 kata.</b>', True, enums.ParseMode.HTML)
                     else:
-                        return await menfess_handler(client, msg, hastag)
+                        return await menfess_handler(client, msg)
                 else:
                     await gagal_kirim_handler(client, msg, hastag)
             else:
