8291aec4fc709eb1bc4552ae14a3c217ab508a68
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-04 00:23:49 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -17,6 +17,22 @@ async def send_menfess_handler(client: Client, msg: Message, key: str, hastag: l
     db = Database(msg.from_user.id)
     db_user = db.get_data_pelanggan()
     db_bot = db.get_data_bot(client.id_bot).kirimchannel
+
+   # Check if the sender has a username
+    if msg.from_user.username is None:
+        return await msg.reply('Anda harus memiliki username untuk mengirim menfess.', quote=True)
+
+    # Check if the message mentions the sender's username
+    username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
+    if not username and username in msg.text.lower():
+        return await msg.reply('⚠️Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
+
+        # Use regular expression to check for links in the message
+        if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
+            return await msg.reply(f"Tidak diizinkan mengirimkan tautan.", quote=True)
+
+
+
     if msg.text or msg.photo or msg.video or msg.voice:
         if msg.photo and not db_bot.photo:
             if db_user.status == 'member' or db_user.status == 'talent':
@@ -39,21 +55,6 @@ async def send_menfess_handler(client: Client, msg: Message, key: str, hastag: l
                     return await msg.reply(f'Pesanmu gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali. Coin mu kurang untuk mengirim menfess diluar batas harian. \n\nwaktu reset jam 1 pagi \n\nKamu dapat mengirim menfess kembali pada esok hari atau top up coin untuk mengirim diluar batas harianmu. \n\n<b>Topup Coin silahkan klik</b> /topup', True, enums.ParseMode.HTML)
 
         link = await get_link()
-    # Check if the sender has a username
-    if msg.from_user.username is None:
-        return await msg.reply('Anda harus memiliki username untuk mengirim menfess.', quote=True)
-
-    # Check if the message mentions the sender's username
-    username = f"@{msg.from_user.username}".lower() if msg.from_user.username else None
-    if not username and username in msg.text.lower():
-        return await msg.reply('⚠️Anda hanya dapat mengirim menfess dengan menggunakan username Anda sendiri.', quote=True)
-
-        # Use regular expression to check for links in the message
-        if re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", msg.text or ""):
-            return await msg.reply(f"Tidak diizinkan mengirimkan tautan.", quote=True)
-
-
-
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
         hapus = await client.delete_message()
 
@@ -75,6 +76,8 @@ async def send_menfess_handler(client: Client, msg: Message, key: str, hastag: l
        disable_web_page_preview=True,        reply_markup=InlineKeyboardMarkup(buttons),
         quote=True
  ),
+    else:
+        await msg.reply('media yang didukung photo, video dan voice')
 
 
 async def get_link():
