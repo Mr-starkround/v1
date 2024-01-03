82c1e330b156c5673c13970e009683b8fa1a46bb
Noob <78058269+Mr-starkround@users.noreply.github.com>
2024-01-03 21:28:17 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -181,8 +181,7 @@ async def on_message(client: Client, msg: Message):
             return
 
 async def hapus_menf(client: Client, query: CallbackQuery):
-     command = msg.text or msg.caption
-        if msg.from_user is None:
+     command = msg.text or msg.caption        
             if msg.sender_chat.id != config.channel_1:
                 return
 
